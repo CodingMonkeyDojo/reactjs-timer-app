1d0b3e2038872e829af935901475fca8a19efa20
Peter Yu <peter1.yu@cibc.com>
2016-12-20 10:23:51 -0500

    Fixed minute hand movement. Introduced dependencies for Jest.

--- src/client/app/components/AnalogDisplay.jsx
+++ src/client/app/components/AnalogDisplay.jsx
@@ -10,11 +10,24 @@ export default class AnalogDisplay extends React.Component {
       stroke: 'rgb(250,0,0)',
       strokeWidth: 2
     }
+
+    let clockRadius = 40
+    let clockX = 50
+    let clockY = 50
+
+    let degree = (this.props.secondsLapsed % 60) / 60 * 360 / 180 * Math.PI
+
+    let offsetX = Math.round(clockRadius * Math.sin(degree))
+    let offsetY = Math.round(clockRadius * Math.cos(degree))
+
     return (
       <div className="analog-display">
+        <div>Angle in (Radian): {degree}</div>
+        <div>Offset X: {offsetX}</div>
+        <div>Offset Y: {offsetY}</div>
         <svg>
-          <circle cx={50} cy={50} r={40} stroke="blue" strokeWidth={4} fill="white" />
-          <line x1={50} y1={50} x2={50 + this.props.secondsLapsed} y2={10} style={lineStyle}></line>
+          <circle cx={clockX} cy={clockY} r={clockRadius} stroke="blue" strokeWidth={4} fill="white" />
+          <line x1={clockX} y1={clockY} x2={clockX + offsetX} y2={clockY - offsetY} style={lineStyle}></line>
         </svg>
       </div>
     )
