e70b4fb5b444f0eec0832405ddeac17bd8f60389
Peter Yu <peter1.yu@cibc.com>
2016-12-21 15:54:50 -0500

    Converted to functional style component.

--- src/client/app/components/DigitalDisplay.jsx
+++ src/client/app/components/DigitalDisplay.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
 
-export default class DigitalDisplay extends React.Component {
-  constructor(props) {
-    super(props)
-    this.formatTime = this.formatTime.bind(this)
-  }
-
-  render() {
-    return (
-      <div className="timer-display">
-        {this.formatTime(this.props.secondsLapsed)}
-      </div>
-    )
-  }
-
-  formatTime(seconds) {
-    return seconds ?
-      `${DigitalDisplay.padUnder60(seconds, 3600)}:${DigitalDisplay.padUnder60(seconds, 60)}:${DigitalDisplay.padUnder60(seconds, 1)}` :
-      '00:00:00'
-  }
-
-  static padUnder60(seconds, units) {
-    let sub60 = Math.floor(seconds/units) % 60;
-    return (sub60 < 10) ? `0${sub60}` : sub60
-  }
-
+let DigitalDisplay = props => {
+  return (
+    <div className="timer-display">
+      {formatTime(props.secondsLapsed)}
+    </div>
+  )
 }
+
+function formatTime(seconds) {
+  return seconds ?
+    `${padUnder60(seconds, 3600)}:${padUnder60(seconds, 60)}:${padUnder60(seconds, 1)}` :
+    '00:00:00'
+}
+
+function padUnder60(seconds, units) {
+  let sub60 = Math.floor(seconds/units) % 60;
+  return (sub60 < 10) ? `0${sub60}` : sub60
+}
+
+export default DigitalDisplay
