5f04bc1efb50a43ad7480e20970d88f60124085b
Peter Yu <peter1.yu@cibc.com>
2016-12-21 09:34:42 -0500

    Converted to functional style component.

--- src/client/app/components/AnalogDisplay.jsx
+++ src/client/app/components/AnalogDisplay.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 
-export default class AnalogDisplay extends React.Component {
-  constructor(props) {
-    super(props)
+let AnalogDisplay = props => {
+  let lineStyle = {
+    stroke: 'rgb(250,0,0)',
+    strokeWidth: 2
   }
 
-  render() {
-    let lineStyle = {
-      stroke: 'rgb(250,0,0)',
-      strokeWidth: 2
-    }
+  let clockRadius = 40
+  let clockX = 50
+  let clockY = 50
 
-    let clockRadius = 40
-    let clockX = 50
-    let clockY = 50
+  let degree = (props.secondsLapsed % 60) / 60 * 360 / 180 * Math.PI
 
-    let degree = (this.props.secondsLapsed % 60) / 60 * 360 / 180 * Math.PI
+  let offsetX = Math.round(clockRadius * Math.sin(degree))
+  let offsetY = Math.round(clockRadius * Math.cos(degree))
 
-    let offsetX = Math.round(clockRadius * Math.sin(degree))
-    let offsetY = Math.round(clockRadius * Math.cos(degree))
-
-    return (
-      <div className="analog-display">
-        <svg>
-          <circle cx={clockX} cy={clockY} r={clockRadius} stroke="blue" strokeWidth={4} fill="white" />
-          <line x1={clockX} y1={clockY} x2={clockX + offsetX} y2={clockY - offsetY} style={lineStyle}></line>
-        </svg>
-      </div>
-    )
-  }
+  return (
+    <div className="analog-display">
+      <svg>
+        <circle cx={clockX} cy={clockY} r={clockRadius} stroke="blue" strokeWidth={4} fill="white" />
+        <line x1={clockX} y1={clockY} x2={clockX + offsetX} y2={clockY - offsetY} style={lineStyle}></line>
+      </svg>
+    </div>
+  )
 }
+
+export default AnalogDisplay
